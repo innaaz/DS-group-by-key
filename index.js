3d9af524ab1224848eb32a9e3a3f4ac1b17fb468
Inna Azarova <inna.azarova.89@gmail.com>
2017-12-27 18:24:47 +0100

    Read all versions of sites and count unique site-key pairs

--- index.js
+++ index.js
@@ -1,60 +1,25 @@
 const fs = require('fs')
 
-const inputDir = '/home/local/download_30-11_0-500_filtered/'
-// const inputDir = './input/'
+// const inputDir = '/home/local/download_30-11_0-500_filtered/'
+const inputDir = './input/'
 
 const fileNames = fs.readdirSync(inputDir)
 console.log(`Number of files: ${fileNames.length}`)
-const siteToFileNameMap = fileNames
-  .sort()
-  .reduce((siteToFileNameMap, fileName, index) => {
-    progress('siteToFileNameMap', index, fileNames.length)
-    const noOUT = fileName.substr(4)
-    const site = noOUT.substr(0, noOUT.indexOf('_'))
-    siteToFileNameMap[site] = fileName
-    return siteToFileNameMap
-  }, {})
-// at this point siteToFileNameMap is:
-// { 
-//   'zztop.com': 'OUT_zztop.com_20171104.json',
-//   'zzu.edu.cn': 'OUT_zzu.edu.cn_20170911.json',
-//   'zzuli.edu.cn': 'OUT_zzuli.edu.cn_20170922.json',
-//   ...
-// }
-// where the json file is the one with the most recent date
 
-const sites = Object.keys(siteToFileNameMap)
-console.log(`Number of sites: ${sites.length}`)
 // Promises are to parallelise reading files
-Promise.all(
-  sites
-    .map((site, index) =>
-      new Promise((resolve, error) => {
-        progress('Requesting readFile', index, sites.length)
-        const fileName = siteToFileNameMap[site]
-        fs.readFile(inputDir + fileName, (err, data) => {
-          if (err) { return error(err) }
-          resolve(data)
-        })
-      }).then(content => {
-        progress('Extracting key', index, sites.length)
-        content = content.toString()
-        if (content.indexOf('"miner": {}') !== -1) { 
-          return false 
-        }
-        const [_, key] = content.match(/^\s+"key"\: "(.+)",$/m) || [0, 0]
-        return key
-      }).catch(error => {
-        console.error(error)
-        return false
-      })
-    )
-).then(keys => {
-  const nonFalsyKeys = keys.filter(key => key)
-  console.log(`Number of sites with miners: ${nonFalsyKeys.length}`)
+readFiles(fileNames).then(siteKeyPairs => {
+  const nonFalsySiteKeyPairs = siteKeyPairs.filter(p => p)
+  console.log(`Number of site versions with miners: ${nonFalsySiteKeyPairs.length}`)
   
-  const keyCounts = nonFalsyKeys
-    .reduce((keyCounts, key) => {
+  const uniqueSiteKeyPairs = nonFalsySiteKeyPairs
+    .reduce((uniq, { site, key }) => {
+      uniq[site + key] = { site, key }
+      return uniq
+    }, {})
+
+  const keyCounts = Object.keys(uniqueSiteKeyPairs)
+    .reduce((keyCounts, i) => {
+      const { key } = uniqueSiteKeyPairs[i]
       keyCounts[key] = (keyCounts[key] || 0) + 1
       return keyCounts
     }, {})
@@ -86,6 +51,34 @@ Promise.all(
   fs.writeFileSync('./data-for-r.json', outputContent)
 })
 
+function readFiles(fileNames) {
+  return Promise.all(
+    fileNames
+      .map((fileName, index) =>
+        new Promise((resolve, error) => {
+          progress('Requesting readFile', index, fileNames.length)
+          fs.readFile(inputDir + fileName, (err, data) => {
+            if (err) { return error(err) }
+            resolve(data)
+          })
+        }).then(content => {
+          progress('Extracting key', index, fileNames.length)
+          content = content.toString()
+          if (content.indexOf('"miner": {}') !== -1) { 
+            return false 
+          }
+          const [_, key] = content.match(/^\s+"key"\: "(.+)",$/m) || [0, false]
+          if (!key) { return false }
+          const noOUT = fileName.substr(4)
+          const site = noOUT.substr(0, noOUT.indexOf('_'))
+          return { site, key }
+        }).catch(error => {
+          console.error(error)
+          return false
+        })
+      )
+  )
+}
 
 function progress (what, index, total) {
   if (index % (Math.round(total / 10)) === 0) {
